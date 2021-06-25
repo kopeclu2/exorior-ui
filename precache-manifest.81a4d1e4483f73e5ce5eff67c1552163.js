dd1b6f1918cd86af065730f2b325127000378f87
Lukas Kopecky <lukas.kopecky.494@gmail.com>
2021-06-25 13:26:56 +0200

    Updates

---
+++ precache-manifest.81a4d1e4483f73e5ce5eff67c1552163.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bef1f4fdf7c32585fb4ec8113037d12b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "191b18c31a42899e2d9c",
+    "url": "./static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "13acb3db6e774d0c7808",
+    "url": "./static/js/2.a68833aa.chunk.js"
+  },
+  {
+    "revision": "6472a7d1d95598048b2f7b02095da167",
+    "url": "./static/js/2.a68833aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "191b18c31a42899e2d9c",
+    "url": "./static/js/main.9d277810.chunk.js"
+  },
+  {
+    "revision": "ff29e0b2eed860eb9e93",
+    "url": "./static/js/runtime-main.85ded171.js"
+  }
+]);
